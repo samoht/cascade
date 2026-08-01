056ada5b880b787868220ba6181cc9f63d4d0576
Thomas Gazagnaire <thomas@gazagnaire.org>
2026-08-01 14:23:53 -0700

    properties: move the filter family into prop_filter
    Only the blend modes, isolation and blur() move: the rest of the filter
    machinery goes through the box-shadow Shadow module, which a later family
    owns.

--- lib/properties.ml
+++ lib/properties.ml
@@ -6,6 +6,7 @@ include Prop_common
 include Prop_svg
 include Prop_multicol
 include Prop_writing
+include Prop_filter
 
 (* Generic length parsing helpers *)
 let read_line_height_length t : line_height =
@@ -7518,33 +7519,6 @@ let rec pp_transform_style : transform_style Pp.t =
   | Revert -> Pp.string ctx "revert"
   | Revert_layer -> Pp.string ctx "revert-layer"
 
-let rec pp_blend_mode : blend_mode Pp.t =
- fun ctx -> function
-  | Normal -> Pp.string ctx "normal"
-  | Multiply -> Pp.string ctx "multiply"
-  | Screen -> Pp.string ctx "screen"
-  | Overlay -> Pp.string ctx "overlay"
-  | Darken -> Pp.string ctx "darken"
-  | Lighten -> Pp.string ctx "lighten"
-  | Color_dodge -> Pp.string ctx "color-dodge"
-  | Color_burn -> Pp.string ctx "color-burn"
-  | Hard_light -> Pp.string ctx "hard-light"
-  | Soft_light -> Pp.string ctx "soft-light"
-  | Difference -> Pp.string ctx "difference"
-  | Exclusion -> Pp.string ctx "exclusion"
-  | Hue -> Pp.string ctx "hue"
-  | Saturation -> Pp.string ctx "saturation"
-  | Color -> Pp.string ctx "color"
-  | Luminosity -> Pp.string ctx "luminosity"
-  | Plus_darker -> Pp.string ctx "plus-darker"
-  | Plus_lighter -> Pp.string ctx "plus-lighter"
-  | Inherit -> Pp.string ctx "inherit"
-  | Initial -> Pp.string ctx "initial"
-  | Unset -> Pp.string ctx "unset"
-  | Revert -> Pp.string ctx "revert"
-  | Revert_layer -> Pp.string ctx "revert-layer"
-  | Var v -> pp_var pp_blend_mode ctx v
-
 let rec pp_text_shadow : text_shadow Pp.t =
  fun ctx -> function
   | None -> Pp.string ctx "none"
@@ -9230,17 +9204,6 @@ let rec pp_nav : nav Pp.t =
   | Revert -> Pp.string ctx "revert"
   | Revert_layer -> Pp.string ctx "revert-layer"
   | Var v -> pp_var pp_nav ctx v
-
-let rec pp_isolation : isolation Pp.t =
- fun ctx -> function
-  | Var v -> pp_var pp_isolation ctx v
-  | Auto -> Pp.string ctx "auto"
-  | Isolate -> Pp.string ctx "isolate"
-  | Initial -> Pp.string ctx "initial"
-  | Inherit -> Pp.string ctx "inherit"
-  | Unset -> Pp.string ctx "unset"
-  | Revert -> Pp.string ctx "revert"
-  | Revert_layer -> Pp.string ctx "revert-layer"
 
 let rec pp_scroll_snap_align : scroll_snap_align Pp.t =
  fun ctx -> function
@@ -13902,20 +13865,6 @@ let rec read_contain t : contain =
     ]
     ~default:read_contain_list t
 
-let rec read_isolation t : isolation =
-  Cursor.enum_or_var "isolation"
-    [
-      ("auto", (Auto : isolation));
-      ("isolate", Isolate);
-      ("initial", Initial);
-      ("inherit", Inherit);
-      ("unset", Unset);
-      ("revert", Revert);
-      ("revert-layer", Revert_layer);
-    ]
-    ~var:(fun t -> Var (Values.read_var read_isolation t))
-    t
-
 let rec read_scroll_behavior (t : Cursor.t) : scroll_behavior =
   Cursor.enum_or_var "scroll-behavior"
     [
@@ -16536,37 +16485,6 @@ let rec read_animation t : animation =
 let read_animations t : animation list =
   Cursor.list ~at_least:1 ~sep:Cursor.comma read_animation t
 
-let rec read_blend_mode t : blend_mode =
-  let read_var t : blend_mode = Var (read_var read_blend_mode t) in
-  Cursor.enum_or_calls "blend-mode"
-    [
-      ("normal", (Normal : blend_mode));
-      ("multiply", Multiply);
-      ("screen", Screen);
-      ("overlay", Overlay);
-      ("darken", Darken);
-      ("lighten", Lighten);
-      ("color-dodge", Color_dodge);
-      ("color-burn", Color_burn);
-      ("hard-light", Hard_light);
-      ("soft-light", Soft_light);
-      ("difference", Difference);
-      ("exclusion", Exclusion);
-      ("hue", Hue);
-      ("saturation", Saturation);
-      ("color", Color);
-      ("luminosity", Luminosity);
-      ("plus-darker", Plus_darker);
-      ("plus-lighter", Plus_lighter);
-      ("inherit", Inherit);
-      ("initial", Initial);
-      ("unset", Unset);
-      ("revert", Revert);
-      ("revert-layer", Revert_layer);
-    ]
-    ~calls:[ ("var", read_var) ]
-    t
-
 module Text_shadow = struct
   type component = Color of color | Length of length
 
@@ -16610,8 +16528,6 @@ let rec read_text_shadow t : text_shadow =
 
 let read_text_shadows t : text_shadow list =
   Cursor.list ~sep:Cursor.comma ~at_least:1 read_text_shadow t
-
-let read_blur t : filter = Cursor.call "blur" t (fun t -> Blur (read_length t))
 
 module Filter = struct
   let read_brightness t : filter =
